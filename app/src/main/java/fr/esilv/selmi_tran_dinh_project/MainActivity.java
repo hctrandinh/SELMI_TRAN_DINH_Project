ef9da586f3fdc6dae164a731f6edab597f614cfe
TRAN-DINH Huân Cyrille <huan@mbpdetrandinh.home>
2020-03-15 17:14:58 +0100

    Bottom nav correction

--- app/src/main/java/fr/esilv/selmi_tran_dinh_project/MainActivity.java
+++ app/src/main/java/fr/esilv/selmi_tran_dinh_project/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends AppCompatActivity{
                     default:
                         return true;
                 }
-                return false;
+                return true;
             }
         });
     }
