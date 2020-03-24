9e616b106f7855ca94c5d6af7f10cf883f6b3623
TRAN-DINH Huân Cyrille <huan@mbpdetrandinh.home>
2020-03-24 23:59:30 +0100

    Back press info

--- app/src/main/java/fr/esilv/selmi_tran_dinh_project/InfoActivity.java
+++ app/src/main/java/fr/esilv/selmi_tran_dinh_project/InfoActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -77,6 +78,13 @@ public class InfoActivity extends AppCompatActivity implements MyRecyclerViewAda
         Intent mainActivity = new Intent(InfoActivity.this, MainActivity.class);
         //String content = Integer.toString((position + 1));
         //mainActivity.putExtra("InfoActivityHomeImg", content);
+        startActivity(mainActivity);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent mainActivity = new Intent(InfoActivity.this, MainActivity.class);
         startActivity(mainActivity);
         finish();
     }
